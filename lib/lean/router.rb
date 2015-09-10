798bdace54a16a5a226d631e86271bb2ccaa7059
Mark Wadham <m@rkw.io>
2015-09-10 22:24:32 +0100

    cleaned up route parsing/matching.

--- lib/lean/router.rb
+++ lib/lean/router.rb
@@ -13,44 +13,23 @@ class Lean::Router
   def self.route
     if routes[Lean::Request.request_method.downcase]
       routes[Lean::Request.request_method.downcase].each do |uri_pattern, route|
-        case uri_pattern
-        when String
-          if uri_pattern == Lean::Request.path
-            return route.split "#"
+        uri = Lean::Request.path.sub(/\A\//,'').sub(/\/\z/,'')
+
+        if m = uri.match(uri_pattern)
+          path = route.split '#'
+          args = []
+
+          for i in 0...path.length
+            for j in 1...m.length
+              path[i].gsub! "$#{j}", m[j]
+            end
+
+            if i >= 2
+              args.push path[i]
+            end
           end
-        when Regexp
-          if (m = Lean::Request.path.sub(/\A\//,'').sub(/\/\z/,'').match uri_pattern)
-            route = route.split "#"
 
-            if route[2]
-              route[2] = [route[2]]
-            end
-
-            if m.length >1
-              for i in 1...m.length
-                regex = Regexp.new(Regexp.escape("$#{i}"))
-                if route[0].match regex
-                  route[0].sub! regex, m[1]
-                  route[0][0] = route[0][0].upcase
-                elsif route[1].match regex
-                  route[1].sub! regex, m[i]
-                end
-              end
-            end
-
-            if route[2]
-              for i in 0...route[2].length
-                for j in 1...m.length
-                  regex = Regexp.new(Regexp.escape("$#{j}"))
-                  if route[2][i].match regex
-                    route[2][i].sub! regex, m[j]
-                  end
-                end
-              end
-            end
-
-            return route
-          end
+          return [path[0], path[1], args]
         end
       end
     end
