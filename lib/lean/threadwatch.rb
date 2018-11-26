0d5c65d163ab009ca35ed011ab002404ee58d815
root <root@rkw.io>
2018-11-26 08:36:25 +0000

    update

--- lib/lean/threadwatch.rb
+++ lib/lean/threadwatch.rb
@@ -1,10 +1,18 @@
 
 class Lean::ThreadWatch
   def initialize
-    hash = scan_dir "."
+    exclude_paths = [
+      'public/',
+      'view/',
+      'log/',
+      'bin/',
+      'run.sh'
+    ]
+
+    hash = scan_dir(exclude_paths)
 
     while 1
-      new_hash = scan_dir "."
+      new_hash = scan_dir(exclude_paths)
 
       if differs hash, new_hash
         exit
@@ -14,14 +22,23 @@ class Lean::ThreadWatch
     end
   end
 
-  def scan_dir(path, hash={})
-    Dir.glob("#{path}/**/*").each do |file|
-      if File.file?(file) and !file.match /^\.\/log\//
+  def scan_dir(exclude_paths, hash={})
+    Dir.glob("**/*").each do |file|
+      if File.file?(file) and !excluded(file, exclude_paths)
         hash[path + "/" + file] = File.stat(path + "/" + file).mtime
       end
     end
 
     hash
+  end
+
+  def excluded(file, exclude_paths)
+    exclude_paths.each do |excluded|
+      if file[0,excluded.length] == excluded
+        return true
+      end
+    end
+    false
   end
 
   def differs(hash, new_hash)
